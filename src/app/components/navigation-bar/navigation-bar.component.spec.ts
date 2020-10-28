c1874cd5653ed0f85812b9c7ba1ec2424b356e2a
Jared Cortez <jared.cortez@xref.com>
2020-10-28 18:13:09 +1100

    Responsive blogs

--- src/app/components/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/components/navigation-bar/navigation-bar.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationBarComponent } from './navigation-bar.component';
 
+import { AppUiModule } from '../common/app-ui.module';
+
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
   let fixture: ComponentFixture<NavigationBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavigationBarComponent ]
+      declarations: [ NavigationBarComponent ],
+      imports: [AppUiModule],
     })
     .compileComponents();
   });
